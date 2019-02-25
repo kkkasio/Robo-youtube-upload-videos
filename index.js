70df6ce423a47221579968f36283398a73237115
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-02-25 14:30:48 -0300

    Feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync')
+
+function start(){
+	const content = {}
+	
+		content.searchTerm = askAndReturnSearchTerm()
+		content.prefix     = askAndReturnPrefix()
+		
+		function askAndReturnSearchTerm(){
+			return readline.question('Digite o termo para buscar no Wikipedia: ')
+		}			
+		
+		function askAndReturnPrefix(){
+			const prefixes = ['Quem e','Oque e','A historia de']
+			const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao: ')
+			const selectedPrefixText  = prefixes[selectedPrefixIndex]
+			
+			return selectedPrefixText
+		}
+			console.log(content)
+	}
+
+start()
